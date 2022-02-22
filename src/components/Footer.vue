54640fedd93982fde299eaec43d0793ecde327aa
juanmaynard91 <juanmaynard91@gmail.com>
2022-02-22 17:47:04 -0300

    Primer commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="page-footer footer">
+    <div class="text-center border-light w-100">
+      <div class="card-header card-header text-dark">
+        <ul class="nav nav-pills card-header-pills">
+          <li class="nav-item">
+            <a target="_blank" class="nav-link bi bi-facebook" href="https://facebook.com/"></a>
+          </li>
+          <li class="nav-item">
+            <a target="_blank" class="nav-link bi bi-youtube" href="https://www.youtube.com/"></a>
+          </li>
+          <li class="nav-item">
+            <a target="_blank" class="nav-link bi bi-instagram" href="https://www.instagram.com/"></a>
+          </li>
+          <li class="nav-item">
+            <a target="_blank" class="nav-link bi bi-linkedin" href="https://ar.linkedin.com/"></a>
+          </li>
+          <li class="nav-item">
+            <a target="_blank" class="nav-link bi bi-github" href="https://github.com"></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style >
+.footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background: black;
+}
+
+.card-header {
+  display: flex;
+  justify-content: center;
+}
+
+.nav-link {
+  color: white;
+}
+</style>
